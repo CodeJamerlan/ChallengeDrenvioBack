fa2f44cbb13a1408a26d645f65ca26aee2c74171
Jaime <Jaimeibarra.ce@gmail.com>
2025-03-18 11:00:17 -0500

    documentación restante

--- src/services/specialOfferService.ts
+++ src/services/specialOfferService.ts
@@ -1,5 +1,11 @@
 import SpecialOffer from "../models/SpecialOffer";
 
+
+/**
+ * servicio refoactorizado para consultar las ofertas especiales mediante el usuario
+ * @param userId el id ddel usuario
+ * @returns retorna los precios especiales filtrados por el usuario
+ */
 export const setSpecialOffersByUser = async (userId:any): Promise<any> => {
     try {
         const userSpecialOffers = await SpecialOffer.find({ userId })
